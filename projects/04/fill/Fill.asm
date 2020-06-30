59d08da33de03fea73f09bbfc4cb0079cb5d279e
ChavezCheong <chavez.cheong@gmail.com>
2020-06-30 15:48:37 +0800

    Chapter 4
    Finished writing two elementary programs in Hack.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,68 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// Pseudo-code
+    
+    @SCREEN     // Set pointer to screen
+    D = A       
+    @screenaddr // screenaddr = 16384
+    M = D
+
+    @KBD        // Set pointer to keyboard
+    D = A
+    @kbdaddr    // kbdaddr = 24576
+    M = D
+
+    @8192
+    D = A
+    @n          // Set n to 8192
+    M = D
+
+(CHECKLOOP)     // Start checking loop
+    @kbdaddr       
+    A = M
+    D = M
+    @ptr
+    M = 0       // Reset the pointer
+    @FILLLOOP
+    D;JNE       // Check if key is pressed
+    @CLEARLOOP
+    D;JEQ       // Check if key is not pressed
+
+(FILLLOOP)    
+    @ptr
+    D = M
+    @n
+    D = D - M
+    @STOP
+    D;JEQ
+    
+    @ptr
+    D = M
+    @screenaddr
+    A = M + D
+    M = -1
+    @ptr
+    M = M + 1
+    @FILLLOOP
+    0;JMP
+(CLEARLOOP)        
+    @ptr
+    D = M
+    @n
+    D = D - M
+    @STOP
+    D;JEQ
+    
+    @ptr
+    D = M
+    @screenaddr
+    A = M + D
+    M = 0
+    @ptr
+    M = M + 1
+    @CLEARLOOP
+    0;JMP
+(STOP)
+    @CHECKLOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+        @sum    //Init the sum
+        M = 0   //Set the sum to 0
+        @itr    //Init the iterator
+        M = 0 
+        @R1
+        D = M
+        @n
+        M = D
+    (LOOP)
+        @itr
+        D = M
+        @n
+        D = D - M
+        @STOP
+        D;JEQ   //if i > n goto STOP
+
+        @R0
+        D = M
+        @sum
+        M = M + D
+        @itr
+        M = M + 1
+        @LOOP
+        0;JMP
+    (STOP)
+        @sum
+        D = M
+        @R2
+        M = D
+    (END)
+        @END
+        0;JMP   //Terminates the program
