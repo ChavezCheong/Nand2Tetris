3ccdb7b51d624f33de15afb040287f2ce0d2250a
ChavezCheong <chavez.cheong@gmail.com>
2020-07-02 23:40:37 +0800

    Chapter 5
    Finally built the CPU.

--- projects/01/repetition.py
+++ projects/01/repetition.py
@@ -1,2 +1,5 @@
-for i in range(8):
-    print(f"Register(in = in, load = load, out = reg{i});")
+#for i in range(8):
+#    print(f"Register(in = in, load = load, out = reg{i});")
+
+a = "0b101111111111111"
+print(a[-14:])
